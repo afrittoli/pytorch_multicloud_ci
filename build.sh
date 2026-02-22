a16db5ee23d945a3a3f15e03d4a8cddb9b18f675
Andrea Frittoli <andrea.frittoli@uk.ibm.com>
2026-02-22 21:08:24 +0000

    Optimise build speed, use lualatex
    Signed-off-by: Andrea Frittoli <andrea.frittoli@uk.ibm.com>

--- build.sh
+++ build.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
 #
 TEX_FILENAME=collaborative_infra
+MODE=${1:-full}  # draft or full
+ENGINE=${2:-lualatex}  # lualatex (default) or xelatex
+
+if [ ! -d tikz-cache ]; then
+  mkdir tikz-cache
+fi
 
 # Reproducible latex builds
-SOURCE_DATE_EPOCH=0 SOURCE_DATE_EPOCH_TEX_PRIMITIVES=1 xelatex ${TEX_FILENAME}.tex &> /dev/null
+if [ "$MODE" = "draft" ]; then
+  OUTPUT=$(SOURCE_DATE_EPOCH=0 SOURCE_DATE_EPOCH_TEX_PRIMITIVES=1 \
+    ${ENGINE} -shell-escape -interaction=nonstopmode "\def\DRAFTMODE{1}\input{${TEX_FILENAME}.tex}" 2>&1)
+  EXIT_CODE=$?
+else
+  OUTPUT=$(SOURCE_DATE_EPOCH=0 SOURCE_DATE_EPOCH_TEX_PRIMITIVES=1 \
+    ${ENGINE} -shell-escape -interaction=nonstopmode ${TEX_FILENAME}.tex 2>&1)
+  EXIT_CODE=$?
+fi
 
-if [ ! $? -eq 0 ]; then
-  echo ${TEX_FILENAME}.tex cannot be compiled
+if [ ! $EXIT_CODE -eq 0 ]; then
+  echo "${TEX_FILENAME}.tex cannot be compiled"
+  echo "$OUTPUT"
+  echo ""
+  echo "Last 30 lines of log file:"
   tail -30 ${TEX_FILENAME}.log
   exit 1
 fi
